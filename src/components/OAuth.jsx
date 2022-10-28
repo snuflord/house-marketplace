dcff82e4b4d3b7de81d2b396aa18fe7c5f4d24c8
snuflord <joebrennan.work@hotmail.co.uk>
2022-10-28 14:57:19 +0100

    facebook added

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,5 +1,5 @@
 import {useLocation, useNavigate} from 'react-router-dom'
-import {getAuth, signInWithPopup, GoogleAuthProvider,} from 'firebase/auth'
+import {getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider} from 'firebase/auth'
 import {doc, setDoc, getDoc, serverTimestamp} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
@@ -39,13 +39,51 @@ function OAuth() {
         }
     }
 
+    const onFBClick = async () => {
+        try {
+            const auth = getAuth()
+            const provider = new FacebookAuthProvider()
+            // allow account selection
+            provider.setCustomParameters({
+                prompt: 'select_account',
+              });
+            // pop up will create a new window instead of redirecting
+            const result = await signInWithPopup(auth, provider)
+            const user = result.user;
+            // check for existing user
+            const docRef = doc(db, 'users', user.uid)
+            const docSnap = await getDoc(docRef)
+            // if user doesn;t exist, create user
+            if(!docSnap.exists()) {
+                await setDoc(doc(db, 'users', user.uid), {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp(),
+                })
+            } 
+            navigate('/')
+        } catch (error) {
+            toast.error('Could not authorise with Facebook')
+        }
+    }
+
   return (
-    <div className="socialLogin">
-        <p>Sign {location.pathname ==='/sign-up' ? 'up' : 'in'} with</p>
-        <button className="socialIconDiv" onClick={onGoogleClick}>
-            <img className='socialIconImg' src={googleIcon} alt="google" />
-        </button>
-    </div>
+    <>
+        <div className="socialLogin">
+            <p>Sign {location.pathname ==='/sign-up' ? 'up' : 'in'} with</p>
+            <button className="socialIconDiv" onClick={onGoogleClick}>
+                <img className='socialIconImg' src={googleIcon} alt="google" />
+            </button>
+        </div>
+        <div className="socialLogin">
+            <p>Sign {location.pathname ==='/sign-up' ? 'up' : 'in'} with</p>
+            <button className="socialIconDiv" onClick={onFBClick}>
+                <img className='socialIconImg' src={googleIcon} alt="fb" />
+            </button>
+        </div>
+    </>
+    
+    
   )
 }
 
